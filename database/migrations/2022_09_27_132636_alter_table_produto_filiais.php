d48e5e153a8a34973187989aefe3e6a99fc9abc2
wilsonluizz <wilsonluiz31051991@gmail.com>
2022-09-27 13:15:30 -0300

    Criação das tabelas e relacionamento
    Criação das tabelas produto_detalhes produto_filiais filiais e unidades.

---
+++ database/migrations/2022_09_27_132636_alter_table_produto_filiais.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produto_filiais', function (Blueprint $table){
+            $table->id();
+            $table->unsignedBigInteger('filial_id');
+            $table->unsignedBigInteger('produto_id');
+            $table->decimal('preco_venda', 8, 2);
+            $table->integer('estoque_minimo');
+            $table->integer('estoque_maximo');
+            $table->timestamps();
+
+            $table->foreign('filial_id')->references('id')->on('filiais');
+            $table->foreign('produto_id')->references('id')->on('produtos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produto_filiais');
+    }
+};
